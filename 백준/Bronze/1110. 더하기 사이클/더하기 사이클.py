f13b67c521b5190ff6f2b989d874a896118c478e
SH-indeyong <118088909+SH-indeyong@users.noreply.github.com>
2024-03-14 06:18:54 +0900

    [Bronze I] Title: 더하기 사이클, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,17 +1,16 @@
 num = int(input())
-if num == 0:
-  print(1)
-  exit()
-
-a = int(num/10)
-b = int(num%10)
-new = 0
+a = num//10
+b = num%10
 count = 0
 
-while new != num:
+while True:
   new = b * 10 + (a + b) % 10
-  a = int(new/10)
-  b = int(new%10)
+  a = new//10
+  b = new%10
   count += 1
+  if num == new: 
+    break
+  else: 
+    continue
 
 print(count)
