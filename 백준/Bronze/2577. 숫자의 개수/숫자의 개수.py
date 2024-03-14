1c941d99b237d30ccd1baa7ae6f1272cc2afa3ae
SH-indeyong <118088909+SH-indeyong@users.noreply.github.com>
2024-03-14 11:39:08 +0900

    [Bronze II] Title: 숫자의 개수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -1,10 +1,4 @@
-a, b, c = int(input()), int(input()), int(input())
+n = int(input()) * int(input()) * int(input())
 
-n = str(a * b * c)
-counts = [0] * 10
-
-for i in n:
-  counts[int(i)] += 1
-
-for j in range(10):
-  print(counts[j])
+for i in range(10):
+  print(str(n).count(str(i)))
