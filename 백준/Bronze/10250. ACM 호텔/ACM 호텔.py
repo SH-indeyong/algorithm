3d2ceef289e62c8d6d22f81db0ddab3cbd5ef983
SH-indeyong <118088909+SH-indeyong@users.noreply.github.com>
2024-03-16 00:55:54 +0900

    [Bronze III] Title: ACM 호텔, Time: 48 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -2,15 +2,8 @@ T = int(input())
 
 for t in range(T):
   h, w, n = map(int, input().split())
-  x, y = 1, 1
   
-  for i in range(n-1):
-    x += 1
-    if x > h:
-      x = 1
-      y += 1
-
-  if len(str(y)) < 2:
-    print(str(x) + '0' + str(y))
+  if n%h == 0:
+    print(h * 100 + n//h)
   else:
-    print(str(x) + str(y))
+    print(n%h * 100 + n//h + 1)
