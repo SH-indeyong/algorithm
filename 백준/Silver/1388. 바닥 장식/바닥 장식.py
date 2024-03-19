b44bc771860d65e3edc7214cc883913c0ffee5ce
SH-indeyong <118088909+SH-indeyong@users.noreply.github.com>
2024-03-19 23:29:15 +0900

    [Silver IV] Title: 바닥 장식, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1388. 바닥 장식/바닥 장식.py
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.py
@@ -6,24 +6,22 @@ for i in range(n):
   floor.append(list(input()))
 
 def dfs_width(x, y):
-  if x < 0 or x >= n or y < 0 or y >= m:
+  if x >= n or y >= m:
     return False
   
   if floor[x][y] == '-':
     floor[x][y] = 0
     dfs_width(x, y + 1)
-    dfs_width(x, y - 1)
     return True
   return False
 
 def dfs_height(x, y):
-  if x < 0 or x >= n or y < 0 or y >= m:
+  if x >= n or y >= m:
     return False
   
   if floor[x][y] == '|':
     floor[x][y] = 0
     dfs_height(x + 1, y)
-    dfs_height(x - 1, y)
     return True
   return False
 
