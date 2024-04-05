cb7bf6f10cc6813af32467b502b83a491216f9ea
SH-indeyong <118088909+SH-indeyong@users.noreply.github.com>
2024-04-06 02:53:29 +0900

    [Silver III] Title: 퇴사, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -5,23 +5,13 @@ P = [0] * N
 for i in range(N):
   T[i], P[i] = map(int, input().split())
 
-def dfs(n, s):
-  global result
+dp = [0] * (N + 1)
 
-  # 종료조건: n을 종료에 관련된 것으로 정의
-  if n >= N:
-    result = max(result, s)
-    return
-  
-  # 상담하는 경우
+for n in range(N - 1, -1, -1):
+  # 기간 내에 상담 가능한 경우
   if n + T[n] <= N:
-    dfs(n + T[n], s + P[n])
+    dp[n] = max(dp[n + 1], dp[n + T[n]] + P[n])
+  else:
+    dp[n] = dp[n + 1]
 
-  # 상담하지 않는 경우
-  dfs(n + 1, s)
-
-result = 0
-# dfs(날짜, 수익)
-dfs(0, 0)
-
-print(result)
+print(dp[0])
